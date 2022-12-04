da6a9af37082f5f137962e8f85ede250395a93d2
kilmanio <f.vastenhout@live.nl>
2022-12-04 17:52:11 +0100

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -8,13 +8,42 @@ pub fn part1(input: &str) -> u32 {
         .sum()
 }
 
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> u32 {
+    let mut iter = input.lines();
+
+    let mut sum = 0;
+
+    //Presumably, the #input is a multiple of 3, so this is safe :^)
+    'outer: while let Some(l1) = iter.next() {
+        let l2 = iter.next().unwrap();
+        let l3 = iter.next().unwrap();
+
+        for c1 in l1.chars() {
+            for c2 in l2.chars() {
+                if c1 == c2 {
+                    for c3 in l3.chars() {
+                        if c1 == c3 {
+                            sum += get_priority(c1);
+                            continue 'outer;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    sum
+}
+
 fn get_deviant(line: &str) -> char {
     let len = line.len();
     let v: Vec<char> = line.chars().collect();
 
-    for c1 in &v[..len/2] {
-        for c2 in &v[len/2..] {
-            if c1 == c2 { return *c1}
+    for c1 in &v[..len / 2] {
+        for c2 in &v[len / 2..] {
+            if c1 == c2 {
+                return *c1;
+            }
         }
     }
     unreachable!()
@@ -22,9 +51,9 @@ fn get_deviant(line: &str) -> char {
 
 fn get_priority(c: char) -> u32 {
     if c.is_lowercase() {
-        (c as u32) - 96
+        (c as u32) - (b'a' as u32) + 1
     } else {
-        (c as u32) - 38 // 65 - 27
+        (c as u32) - (b'A' as u32) + 27
     }
 }
 
