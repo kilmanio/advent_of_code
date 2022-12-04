7882ddbdfea9eb02e454604f1f4df0c984cd072f
kilmanio <f.vastenhout@live.nl>
2022-12-04 11:36:33 +0100

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -2,15 +2,30 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> u32 {
-    let (elf_number, elf_calories) = input
+    let elf_calories = input
         .split("\n\n")
         .map(|elf| {
-            elf.split("\n")
+            elf.split('\n')
                 .map(|calories| calories.parse::<u32>().unwrap())
                 .sum::<u32>()
         })
-        .enumerate()
-        .max_by(|(_, a), (_, b)| a.cmp(b))
+        .max_by(|a, b| a.cmp(b))
         .unwrap();
     elf_calories
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> u32 {
+    let mut calories: Vec<u32> = input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect();
+
+    calories.sort();
+
+    calories.pop().unwrap() + calories.pop().unwrap() + calories.pop().unwrap()
 }
