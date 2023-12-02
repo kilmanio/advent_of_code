829a47d2f6331e8c5179c1a6b8a67a5bfa07d241
Floris Vastenhout <kilmanio@mailfence.com>
2023-12-02 22:22:20 +0100

    Add y2023d1p1

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -1,8 +1,20 @@
 use aoc_runner_derive::aoc;
 
+fn do_line(input: &str) -> u32 {
+    let mut i = input.chars()
+        .filter(|c| c.is_ascii_digit())
+        .map(|c| c.to_digit(10).unwrap());
+
+    let tens = i.next().unwrap();
+
+    tens*10+i.last().unwrap_or(tens)
+}
+
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> u32 {
-    todo!()
+    input.split("\n")
+        .map(|l| do_line(l))
+        .sum()
 }
 
 #[aoc(day1, part2)]
