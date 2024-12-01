4a6926658c3ccdd8f7d49f196ebea53cbbb030d4
Floris Vastenhout <kilmanio@mailfence.com>
2024-12-01 16:56:31 +0100

    Add y2024d1p2

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -1,21 +1,33 @@
 use aoc_runner_derive::aoc;
 
-#[aoc(day1, part1)]
-pub fn part1(input: &str) -> u32 {
+pub fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
     let mut i = 0;
-    let (mut left, mut right): (Vec<i32>, Vec<i32>) = input
+    input
         .split_whitespace()
         .map(|s| s.parse().unwrap())
-        .partition(|_: &i32| {
+        .partition(|_: &u32| {
             i += 1;
-            i % 2 == 0
-        });
+            i % 2 == 1
+        })
+}
 
+#[aoc(day1, part1)]
+pub fn part1(input: &str) -> u32 {
+    let (mut left, mut right) = parse_input(input);
     left.sort();
     right.sort();
 
     left.iter()
         .zip(right.iter())
         .map(|(a, b)| a.abs_diff(*b))
+        .sum()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> u32 {
+    let (left, right) = parse_input(input);
+
+    left.iter()
+        .map(|a| right.iter().filter(|b| a == *b).count() as u32 * a)
         .sum()
 }
