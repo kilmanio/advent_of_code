b928525c938fa070053a1d5baa38dcd4aa2e0cfa
Floris Vastenhout <kilmanio@mailfence.com>
2024-12-01 15:25:35 +0100

    Add y2024d1p1

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -2,5 +2,20 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> u32 {
-    todo!()
+    let mut i = 0;
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .partition(|_: &i32| {
+            i += 1;
+            i % 2 == 0
+        });
+
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .zip(right.iter())
+        .map(|(a, b)| a.abs_diff(*b))
+        .sum()
 }
