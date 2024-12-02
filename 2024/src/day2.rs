3f376d8e8353b2e31a9d84a1610cef06232df862
Floris Vastenhout <kilmanio@mailfence.com>
2024-12-02 14:49:34 +0100

    Rewrite y2024d2p1

--- 2024/src/day2.rs
+++ 2024/src/day2.rs
@@ -1,24 +1,40 @@
 use aoc_runner_derive::aoc;
 use std::cmp::Ordering;
 
-fn check_safe(v: &[u32]) -> bool {
+#[derive(Eq, PartialEq)]
+enum Direction {
+    Unsafe,
+    Up,
+    Down,
+}
+
+fn check_direction(lhs: &u32, rhs: &u32) -> Direction {
+    let diff = lhs.abs_diff(*rhs);
+    if diff > 3 {
+        return Direction::Unsafe;
+    }
+
+    match lhs.cmp(rhs) {
+        Ordering::Less => Direction::Down,
+        Ordering::Equal => Direction::Unsafe,
+        Ordering::Greater => Direction::Up,
+    }
+}
+
+fn check_record(v: &[u32]) -> bool {
     let mut it = v.iter();
     let mut lhs = it.next().unwrap();
-    let mut ordering = Ordering::Equal;
+    let mut direction: Option<Direction> = None;
 
     for rhs in it {
-        let this_ordering = lhs.cmp(rhs);
-
-        if (this_ordering as i8).abs_diff(ordering as i8) > 1 {
-            return false;
-        }
-        ordering = this_ordering;
-
-        let diff = lhs.abs_diff(*rhs);
-        if diff < 1 || diff > 3 {
-            return false;
+        let this_direction = check_direction(lhs, rhs);
+        if let Some(dir) = direction {
+            if this_direction != dir {
+                return false;
+            }
         }
 
+        direction = Some(this_direction);
         lhs = rhs;
     }
 
@@ -34,6 +50,6 @@ pub fn part1(input: &str) -> u32 {
                 .map(|s2| s2.parse::<u32>().unwrap())
                 .collect::<Vec<u32>>()
         })
-        .filter(|v| check_safe(v))
+        .filter(|v| check_record(v))
         .count() as u32
 }
